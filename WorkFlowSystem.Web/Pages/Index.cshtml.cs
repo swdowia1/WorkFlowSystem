3bbcc27807f92f319690b74b3d95cdfdebec1cf9
swdowia1 <swdowia1@gazeta.pl>
2026-07-20 19:22:10 +0200

    dodaj worklog

--- WorkFlowSystem.Web/Pages/Index.cshtml.cs
+++ WorkFlowSystem.Web/Pages/Index.cshtml.cs
@@ -10,15 +10,37 @@ namespace WorkFlowSystem.Web.Pages
     {
       
          private readonly TaskService _TaskService;
- 
+        private readonly WorkLogService _workLogService;
         public List<TaskProjectDto> Tasks { get; set; } = [];
 
-        public IndexModel( TaskService taskService)
+        public IndexModel(TaskService taskService, WorkLogService workLogService)
         {
-           
-            _TaskService = taskService;
-        }
 
+            _TaskService = taskService;
+            _workLogService = workLogService;
+        }
+        public async Task<IActionResult> OnPostAddWorkLogAsync(
+ [FromBody] WorkLogDto request)
+        {
+            if (request.Hours <= 0)
+            {
+                return BadRequest("Nie wybrano czasu.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Description))
+            {
+                return BadRequest("Opis jest wymagany.");
+            }
+
+
+
+            await _workLogService.AddAsync(request);
+
+            return new JsonResult(new
+            {
+                success = true
+            });
+        }
         public async Task OnGet()
         {
       
--- WorkFlowSystem.Web/Pages/Tasks/Index.cshtml.cs
+++ WorkFlowSystem.Web/Pages/Tasks/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkFlowSystem.Application.DTO;
 using WorkFlowSystem.Application.Services;
 using WorkFlowSystem.Domain.Entities;
 
@@ -8,18 +9,42 @@ namespace WorkFlowSystem.Web.Pages.Tasks
     public class IndexModel : PageModel
     {
         private readonly TaskService _service;
+        private readonly WorkLogService _workLogService;
 
         public List<TaskItem> Tasks { get; set; } = [];
 
-        public IndexModel(TaskService service)
+        public IndexModel(TaskService service, WorkLogService workLogService)
         {
             _service = service;
+            _workLogService = workLogService;
         }
 
         public async Task OnGetAsync()
         {
            // Tasks = await _service.GetAllAsync();
             Tasks = await _service.GetListAsync();
+        }
+        public async Task<IActionResult> OnPostAddWorkLogAsync(
+ [FromBody] WorkLogDto request)
+        {
+            if (request.Hours <= 0)
+            {
+                return BadRequest("Nie wybrano czasu.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Description))
+            {
+                return BadRequest("Opis jest wymagany.");
+            }
+
+
+
+            await _workLogService.AddAsync(request);
+
+            return new JsonResult(new
+            {
+                success = true
+            });
         }
     }
 }
