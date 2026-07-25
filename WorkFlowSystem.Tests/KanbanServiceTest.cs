94953414daa4e78aaba0626d0c8732c6b4e7c5c9
swdowia1 <swdowia1@gazeta.pl>
2026-07-25 08:11:53 +0200

    test

---
+++ WorkFlowSystem.Tests/KanbanServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkFlowSystem.Application.Services;
+
+namespace WorkFlowSystem.Tests
+{
+    public class KanbanServiceTest : ServiceTestBase<KanbanService>
+    {
+        [Fact]
+        public async Task TaskSatusTest()
+        {
+            contextDB.Tasks.Add(new Domain.Entities.TaskItem   
+            {
+                Id = 1,
+                Title = "Task 1",
+                ProjectId = 1,
+                Status = Domain.Enums.TaskProjectStatus.New
+
+            });
+            await contextDB.SaveChangesAsync();
+
+            await service.UpdateStatusAsync(new Application.DTO.UpdateTaskStatusDto
+            {
+                TaskId = 1,
+                Status = (int)Domain.Enums.TaskProjectStatus.Done
+            }); 
+
+            var ttask = await contextDB.Tasks.FindAsync(1);
+            ttask.Id.Should().Be(1);
+
+        }
+    }
+}
---
+++ WorkFlowSystem.Tests/ServiceTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WorkFlowSystem.Application.Services;
+using WorkFlowSystem.Infrastructure.Persistence;
+using WorkFlowSystem.Tests.KLasy;
+
+namespace WorkFlowSystem.Tests
+{
+    public abstract class ServiceTestBase<TService>
+    where TService : class, IService
+    {
+        protected IServiceProvider provider { get; }
+        protected ApplicationDbContext contextDB { get; }
+        protected TService service { get; }
+
+        protected ServiceTestBase()
+        {
+            provider = TestServiceProvider.Create();
+
+            contextDB = provider.GetRequiredService<ApplicationDbContext>();
+            service = provider.GetRequiredService<TService>();
+        }
+    }
+}
