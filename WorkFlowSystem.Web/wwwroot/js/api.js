67faf23a508bae2d88cd51b4973b3e66503c7442
swdowia1 <swdowia1@gazeta.pl>
2026-07-28 08:13:25 +0200

    wstepne usuwanie

--- WorkFlowSystem.Web/wwwroot/js/api.js
+++ WorkFlowSystem.Web/wwwroot/js/api.js
@@ -1,12 +1,10 @@
-﻿// api.js
-
-function getToken() {
+﻿function getToken() {
     return document.querySelector(
         'input[name="__RequestVerificationToken"]'
     ).value;
 }
 
-async function request(method, handler, body = null) {
+async function request(method, handler, body = null, reload = false) {
 
     const options = {
         method,
@@ -26,6 +24,11 @@ async function request(method, handler, body = null) {
         throw new Error(await response.text());
     }
 
+    if (reload) {
+        location.reload();
+        return;
+    }
+
     if (response.status === 204) {
         return null;
     }
@@ -38,13 +41,13 @@ window.api = {
         return request("GET", handler);
     },
 
-    post(handler, data) {
-        console.log("POST", handler, data);
-        return request("POST", handler, data);
+    post(handler, data, reload = false) {
+        console.log("POST", handler, data, "reload", reload);
+        return request("POST", handler, data, reload);
     },
 
-    delete(handler, id) {
+    delete(handler, id, reload = false) {
         console.log("DELETE", handler, id);
-        return request("POST", handler, { id });
+        return request("POST", handler, { id }, reload);
     }
 };
