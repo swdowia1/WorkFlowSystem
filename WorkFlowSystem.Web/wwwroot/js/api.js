4f4be73c882701557fcd34a1a12daae4d14c4bb6
swdowia1 <swdowia1@gazeta.pl>
2026-07-30 09:25:01 +0200

    dodawwanie wroklog

--- WorkFlowSystem.Web/wwwroot/js/api.js
+++ WorkFlowSystem.Web/wwwroot/js/api.js
@@ -4,21 +4,23 @@
     ).value;
 }
 
-async function request(method, handler, body = null, reload = false) {
+async function request(method, url, body = null, reload = false, antiforgery = true) {
 
     const options = {
         method,
-        headers: {
-            "RequestVerificationToken": getToken()
-        }
+        headers: {}
     };
+
+    if (antiforgery) {
+        options.headers["RequestVerificationToken"] = getToken();
+    }
 
     if (body) {
         options.headers["Content-Type"] = "application/json";
         options.body = JSON.stringify(body);
     }
 
-    const response = await fetch(`?handler=${handler}`, options);
+    const response = await fetch(url, options);
 
     if (!response.ok) {
         throw new Error(await response.text());
@@ -36,18 +38,50 @@ async function request(method, handler, body = null, reload = false) {
     return await response.json();
 }
 
+
 window.api = {
+
     get(handler) {
-        return request("GET", handler);
+        return request(
+            "GET",
+            `?handler=${handler}`
+        );
     },
 
     post(handler, data, reload = false) {
         console.log("POST", handler, data, "reload", reload);
-        return request("POST", handler, data, reload);
+
+        return request(
+            "POST",
+            `?handler=${handler}`,
+            data,
+            reload
+        );
     },
+
+
+    // Web API POST
+    postApi(url, data, reload = false) {
+        console.log("POST API", url, data, "reload", reload);
+
+        return request(
+            "POST",
+            url,
+            data,
+            reload,
+            false
+        );
+    },
+
 
     delete(handler, id, reload = false) {
         console.log("DELETE", handler, id);
-        return request("POST", handler, { id }, reload);
+
+        return request(
+            "POST",
+            `?handler=${handler}`,
+            { id },
+            reload
+        );
     }
 };
